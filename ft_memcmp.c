3096cd4c9171c761de1208299fbfa54abbb8deeb
Jeremy Maia <jmaia@student.42.fr>
2021-11-23 09:09:48 +0100

    Create ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 09:04:55 by jmaia             #+#    #+#             */
+/*   Updated: 2021/11/23 09:09:38 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const char	*str1;
+	const char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	return (ft_strncmp(str1, str2, n));
+}
