63947cfd2d7cea35212d0158ee56ba1d02dd21a4
Jeremy Maia <jmaia@student.42.fr>
2021-11-23 14:17:29 +0100

    Add atoi to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 12:50:43 by jmaia             #+#    #+#             */
-/*   Updated: 2021/11/22 13:49:52 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/11/23 14:16:40 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int		ft_strncmp(const char *s1, const char s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmembm, size_t size);
 char	*ft_strdup(const char *s);
 
