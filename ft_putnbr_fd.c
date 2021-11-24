6f09a2a0421c4c4770cf3fef95cd35c721ff8fa1
Jeremy Maia <jmaia@student.42.fr>
2021-11-24 15:29:03 +0100

    Create ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 15:07:38 by jmaia             #+#    #+#             */
+/*   Updated: 2021/11/24 15:28:38 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+static void	ft_putnbr_positive_fd(unsigned int n, int fd)
+{
+	char	c;
+
+	if (n < 10)
+	{
+		c = n + '0';
+		write(fd, &n, 1);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+		write(fd, "-", 1);
+	ft_putnbr_positive_fd((unsigned int) n, fd);
+}
