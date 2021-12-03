f28befa39043bafe8f275a737c7f0beb459558cb
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 12:41:55 +0100

    Protect ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:16:37 by jmaia             #+#    #+#             */
-/*   Updated: 2021/11/25 21:14:06 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/11/29 13:13:20 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,27 @@ static char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-static void	insert_word(char **splitted, int start, int end, char *str)
+static int	insert_word(char **splitted, int start, int end, char *str)
 {
 	char	*word;
 
 	word = malloc(sizeof(*word) * (end - start + 2));
+	if (!word)
+		return (0);
 	ft_strncpy(word, (char *)(str + start), end - start + 2);
 	word[end - start + 1] = 0;
 	*splitted = word;
+	return (1);
+}
+
+static void	free_everything(char **splitted, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+		free(splitted[i]);
+	free(splitted);
 }
 
 char	**ft_split(char *str, char c)
@@ -61,22 +74,23 @@ char	**ft_split(char *str, char c)
 	int		i;
 	int		i_splitted;
 	char	**splitted;
+	int		has_error;
 
 	splitted = malloc(sizeof(*splitted) * (count_words(str, c) + 1));
 	if (splitted == 0)
 		return (0);
-	start = -1;
-	i = -1;
-	i_splitted = 0;
+	splitted = (splitted++, start = -1, i = -1, i_splitted = 0, splitted--,
+			splitted);
 	while (str[++i])
 	{
 		if (str[i] != c && start == -1)
 			start = i;
 		if ((str[i + 1] == c || !str[i + 1]) && start != -1)
 		{
-			insert_word(splitted + i_splitted, start, i, str);
-			++i_splitted;
-			start = -1;
+			has_error = !insert_word(splitted + i_splitted, start, i, str);
+			if (has_error)
+				free_everything(splitted, i_splitted + 1);
+			start = (start++, ++i_splitted, start = -1, start);
 		}
 	}
 	splitted[i_splitted] = 0;
