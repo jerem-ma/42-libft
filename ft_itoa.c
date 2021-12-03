98af544e0c725a1b380f463bd4fa437e4dd9573c
Jeremy Maia <jmaia@student.42.fr>
2021-12-03 14:12:43 +0100

    Create ft_itoa_base()

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,85 +6,13 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:44:22 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/03 14:03:33 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/03 14:12:03 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-int	get_length(unsigned int nb, int base)
-{
-	int	length;
-	int	temp_count;
-
-	length = 0;
-	temp_count = nb;
-	while (temp_count != 0)
-	{
-		temp_count /= base;
-		length++;
-	}
-	if (nb == 0)
-		length = 1;
-	return (length);
-}
-
-char	get_char(int n, char *base)
-{
-	return (base[n]);
-}
-
-long	power(int x, int pow)
-{
-	long	result;
-	int		n;
-
-	result = 1;
-	n = 0;
-	while (n++ < pow)
-	{
-		result *= x;
-	}
-	return (result);
-}
-
-char	*fill_number(int nb_len, long nb, char *base, int base_len)
-{
-	int		i;
-	int		digit;
-	int		str_len;
-	char	*itoad;
-
-	i = -1;
-	str_len = nb_len + (nb < 0) + 1;
-	itoad = malloc(sizeof(*itoad) * str_len);
-	if (itoad == 0)
-		return (0);
-	if (nb < 0)
-	{
-		itoad[0] = '-';
-		nb *= -1;
-		++i;
-	}
-	while (++i < str_len - 1)
-	{
-		digit = nb / power(base_len, nb_len - 1);
-		itoad[i] = get_char(digit, base);
-		nb -= digit * power(base_len, nb_len - 1);
-		nb_len--;
-	}
-	itoad[i] = 0;
-	return (itoad);
-}
+#include "libft.h"
 
 char	*ft_itoa(long nbr)
 {
-	int		origin_length;
-	int		length_base;
-	char	*base;
-
-	base = "0123456789";
-	length_base = 10;
-	origin_length = get_length(nbr, length_base);
-	return (fill_number(origin_length, nbr, base, length_base));
+	return (ft_itoa_base(nbr, "0123456789"));
 }
